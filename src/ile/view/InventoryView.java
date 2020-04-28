9fcc16a17807aca00816f12eda8f14a99939d208
Gozea <rbournet@gmail.com>
2020-04-28 19:16:02 +0200

    pouet

--- src/ile/view/InventoryView.java
+++ src/ile/view/InventoryView.java
@@ -8,36 +8,40 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.imageio.ImageIO;
 
 
 public class InventoryView extends JPanel implements Observer {
     private Model model;
     private final static int WIDTH = 50*Model.LONGUEUR;
-    private final static int HEIGHT = 200;
+    private final static int HEIGHT = 40*Model.LONGUEUR;
 
-    private BufferedImage keyWater = ImageIO.read(new File("src/files/kwater.JPG"));
-    private BufferedImage keyFire = ImageIO.read(new File("src/files/kfire.JPG"));
-    private BufferedImage keyAir = ImageIO.read(new File("src/files/kair.JPG"));
-    private BufferedImage keyEarth = ImageIO.read(new File("src/files/kearth.JPG"));
+    private BufferedImage keyWater = ImageIO.read(new File("/home/gozea/IleInterdite2/ileinterdite/src/files/kwater.JPG"));
+    private BufferedImage keyFire = ImageIO.read(new File("/home/gozea/IleInterdite2/ileinterdite/src/files/kfire.JPG"));
+    private BufferedImage keyAir = ImageIO.read(new File("/home/gozea/IleInterdite2/ileinterdite/src/files/kair.JPG"));
+    private BufferedImage keyEarth = ImageIO.read(new File("/home/gozea/IleInterdite2/ileinterdite/src/files/kearth.JPG"));
 
     private JLabel title;
-    private JLabel player1;
-    private JLabel player2;
-    private JLabel player3;
-    private JLabel player4;
+    private ArrayList<JLabel> players;
 
     public InventoryView(Model model) throws IOException {
         this.model = model;
+        this.setLayout(null);
         model.addObserver(this); //pas sur
         Dimension dim = new Dimension(WIDTH, HEIGHT);
         this.setPreferredSize(dim);
-        this.setLayout(null);
         this.title = new JLabel("Inventaire");
         this.add(this.title);
         Dimension size = this.title.getPreferredSize();
-        this.title.setBounds(WIDTH/2, HEIGHT/2 , size.width, size.height);
+        this.title.setBounds(WIDTH/2, HEIGHT/10 , size.width, size.height);
         this.title.setVisible(true);
+        this.players = new ArrayList<>();
+        for (int i = 0 ; i < this.model.getPlayers().size() ; i++) {
+            this.players.add(new JLabel("Joueur " + i));
+            this.add(this.players.get(i));
+            this.players.get(i).setBounds(WIDTH/15, (i+1)*HEIGHT/this.players.size(), size.width, size.height);
+        }
     }
 
     @Override
